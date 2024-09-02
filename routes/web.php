bf55b392a8bcc185d2200c0f8a9de58a864daaaf
Silmymaulia <silmy.smd@gmail.com>
2024-09-02 19:11:50 +0700

    Pertemuan 2 - Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -123,3 +123,14 @@ Route::resource('photos', PhotoController::class);
 Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Silmy']);
+// });
+
+// //perubahan
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Silmy']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
